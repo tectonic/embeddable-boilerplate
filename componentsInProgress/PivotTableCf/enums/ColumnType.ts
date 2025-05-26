6edf2d485f4dd19b475490ab4d8ac12a51236c20
jc-creativeforce <jc@creativeforce.team>
2025-05-26 09:40:17 +0800

    Update reference old components

--- componentsInProgress/PivotTableCf/enums/ColumnType.ts
+++
@@ -1,6 +0,0 @@
-export enum ColumnType {
-  DIMENSION = 'dimension',
-  MEASURE = 'measure',
-  ROW_HEADER_GROUP = 'row-header-group',
-  ROW_HEADER = 'row-header'
-}
--- componentsInProgress/PivotTableCf/enums/MeasureVisualizationFormat.ts
+++
@@ -1,5 +0,0 @@
-export enum MeasureVisualizationFormat {
-  NUMERIC_VALUES_ONLY = 'Numeric Value Only',
-  VALUE_BARS = 'Value Bars',
-  HEATMAP = 'Heat Map'
-}
