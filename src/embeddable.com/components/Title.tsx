6817216607fdfdf31e9271afcd21570845bce3d8
jc-creativeforce <jc@creativeforce.team>
2025-05-27 16:21:09 +0800

    Set font weight of table title

--- src/embeddable.com/components/Title.tsx
+++ src/embeddable.com/components/Title.tsx
@@ -9,7 +9,7 @@ export default function Title({ title, style }: Props) {
   return (
     !!title && (
       <h2
-        className="w-full text-[#333942] text-base font-bold font-embeddable justify-start flex leading-6 mb-2"
+        className="w-full text-[#333942] text-base font-[--embeddable-charts-fontWeights-title] font-embeddable justify-start flex leading-6 mb-2"
         style={style || {}}
       >
         {title}
